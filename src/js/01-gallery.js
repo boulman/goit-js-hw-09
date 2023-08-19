f2c5015a5b88518d700ca6c93ff584a56116bac1
Anton_Lisoval <lisowal@gmail.com>
2023-08-19 16:20:10 +0300

    3 tasks done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallary = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+  const markup = arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+  gallary.insertAdjacentHTML('beforeend', markup);
+}
+createMarkup(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallary.addEventListener('click', handleClick);
+
+function handleClick(e) {
+  e.preventDefault();
+  if (e.target.className !== 'gallery__image') {
+    return;
+  }
+  const { source } = e.target.dataset;
+
+  console.log;
+  gallery.on('show.simplelightbox');
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const CURRENT_PLAYER_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('loaded', function () {
+  const currentTime = localStorage.getItem(CURRENT_PLAYER_TIME);
+  if (currentTime) {
+    player.setCurrentTime(Number(JSON.parse(currentTime)));
+  }
+});
+
+player.on(
+  'timeupdate',
+  throttle(function () {
+    player
+      .getCurrentTime()
+      .then(sec =>
+        localStorage.setItem(CURRENT_PLAYER_TIME, JSON.stringify(sec))
+      );
+  }, 1000)
+);
